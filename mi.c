eb379e9d46d74ba860f7333d3e0b6206ce1351b9
balakutty <35652869+balakutty@users.noreply.github.com>
2018-01-26 10:22:05 +0000

    Create mi.c

---
+++ mi.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+char letter;
+scanf("%c",&letter);
+if(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u')
+printf("the given charecter is vowel");
+else
+printf("the given charecter is consonent");
+}
