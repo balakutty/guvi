352d127857d28a9791dbd45a2ccdfa33b3801975
balakutty <35652869+balakutty@users.noreply.github.com>
2018-02-08 16:33:32 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+    inta,b,c;
+    printf("\n enter the value of a");
+    scanf("%d",&a);
+    printf("\n enter the value of b");
+    scanf("%d",&b);
+    c=(a^b)
+    scanf("%d",&c);
+}
