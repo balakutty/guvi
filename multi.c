64543841b49cd25474ad3f8246c5535738ad9ffa
balakutty <35652869+balakutty@users.noreply.github.com>
2018-02-09 16:41:07 +0530

    Create multi.c

---
+++ multi.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,a;
+    printf("\n enter the  value of a");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        (a=i*5)
+    printf(" %d",a);
+    }
+}
