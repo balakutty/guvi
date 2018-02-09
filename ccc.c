970636dd680afb28b70fcf0184860b4b6069d428
balakutty <35652869+balakutty@users.noreply.github.com>
2018-02-09 15:59:26 +0530

    Create ccc.c

---
+++ ccc.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the value of a ");
+    scanf("%d",&a);
+    if
+    {
+        (a/1)(a/a)
+        printf("it is prime number");
+    
+    else
+{
+    printf("not a prime number");
+}
+}
+        
+    }
+    }
