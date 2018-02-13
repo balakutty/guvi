963a799894b111c575d51ca3fa4ad482dd85042b
balakutty <35652869+balakutty@users.noreply.github.com>
2018-02-13 16:17:37 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,i;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    for(i=1;i<10;i++)
+    if
+    {
+        (a/2=!0)
+        printf("it is a prime number =%d");
+        else
+        {
+            (a/2=0)
+            printf("it is not  prime number =%d");
+        }
+    }
+}
