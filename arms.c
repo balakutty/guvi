d3bd37d1c44ab46fbf75bf96a654125cc6b99dcd
balakutty <35652869+balakutty@users.noreply.github.com>
2018-02-13 16:54:59 +0530

    Create arms.c

---
+++ arms.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int t,a,b,c;
+    printf("enter the value of t,a,b,c");
+    scanf("%d %d %d %d",&t,&a,&b,&c);
+    if
+    {
+        ((a*a*a)+(b*b*b)+(c*c*c))=t
+        printf("it is armstrong number");
+        else
+        {
+            ((a*a*a)+(b*b*b)+(c*c*c))=!0
+            printf("it is not a armstrong number");
+        }
+    }
+}
