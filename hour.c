ea6715446bca37450852e6d59facf808906e1bfb
balakutty <35652869+balakutty@users.noreply.github.com>
2018-03-01 16:24:22 +0530

    Create hour.c

---
+++ hour.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    c=(a*60)+b;
+    printf("total numbers is = %d",c);
+}
