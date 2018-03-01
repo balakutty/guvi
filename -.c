1940279d4fbcccc2c0f7bb15f053e099ad534891
balakutty <35652869+balakutty@users.noreply.github.com>
2018-03-01 16:40:39 +0530

    Create -.c

---
+++ -.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    float a,b,c;
+    printf("enter the  value of a");
+    scanf("%f",&a);
+    printf("enter the value of b");
+    scanf("%f",&b);
+    c=(a-b);
+    scanf("%f",&c);
+}
