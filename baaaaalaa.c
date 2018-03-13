4d1261a37119bd85dc4dbc8e272d31f3d19576a7
bala mahi <35652869+balakutty@users.noreply.github.com>
2018-03-13 16:02:07 +0530

    Create baaaaalaa.c

---
+++ baaaaalaa.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	int x,i;
+	scanf("%d",&x);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<x;i++)
+	{
+		printf("\n%d %d",i,a[i]);
+	}
+return 0;
+}
